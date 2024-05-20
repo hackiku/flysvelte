88fd7fa04ed4479da5d9639450587e36c80006ca
hackiku <ivan@pipewriter.io>
2024-05-20 21:34:28 +0200

    install tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  
+</script>
+
+<main class="bg-black w-screen h-screen">
+
+</main>
+
