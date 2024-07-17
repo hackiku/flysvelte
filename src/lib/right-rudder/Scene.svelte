35e798182d311bbae80d3e0971adfd06340801fa
hackiku <ivan@pipewriter.io>
2024-07-17 05:14:45 +0200

    reset btn, cleanup

--- src/lib/right-rudder/Scene.svelte
+++ src/lib/right-rudder/Scene.svelte
@@ -1,7 +1,6 @@
 <!-- src/lib/right-rudder/Scene.svelte -->
 
 <script lang="ts">
-  // import { PlaneGeometry, MeshStandardMaterial } from 'three'
   // import { DEG2RAD } from 'three/src/math/MathUtils.js'
 	import { T } from '@threlte/core';
   import { Grid, OrbitControls } from '@threlte/extras';
@@ -9,12 +8,12 @@
   import RAPIER from '@dimforge/rapier3d-compat'
 	import { PlaneGeometry, BoxGeometry, MeshStandardMaterial } from 'three'
 	
-	// sim components
+	// phys components
 	import Ground from './phys/Ground.svelte';
 	import Particle from './phys/Particle.svelte';
   import Model from './models/WING.svelte';
-  import { World, RigidBodyDesc, ColliderDesc } from '@dimforge/rapier3d-compat';
   
+	// stores
 	import { physicsEnabled } from './stores';
   let nsubdivs = 10
   let heights = []
@@ -49,31 +48,34 @@
   />
 </T.PerspectiveCamera>
 
-<!-- Lighting setup -->
+<!-- Lighting -->
 <T.DirectionalLight intensity={0.8} position={[10, 10, 10]} />
 <T.AmbientLight intensity={0.3} />
 
-<!-- {#key resetCounter}
-	<FallingShapes />
-{/key} -->
+{#key resetCounter}
+	<Particle />
+{/key}
 
 <!-- grid -->
-<Grid position.y={-0.0} cellColor="#ffffff" sectionColor="#ffffff" sectionThickness={1} fadeDistance={105} cellSize={2} />
+<Grid position.y={0.01} cellColor="#ffffff" sectionColor="#ffffff" sectionThickness={1} fadeDistance={105} cellSize={2} />
 
-<!-- <T.Mesh ref={boxMesh}> -->
-<T.Mesh>
+
+<T.Mesh position={[1, 3, 0]} rotation={[0.5, 0.5, 0]} scale={[1, 1, 1]}>
+  <T.BoxGeometry args={[1, 1, 1]} />
   <T.MeshStandardMaterial
-    color="teal"
-    opacity="0.9"
-    transparent
+    color="#00ff00"
+    metalness={0.7}
+    roughness={0.2}
+    emissive="#0000ff"
+    emissiveIntensity={0.5}
+    opacity={0.8}
+    transparent={true}
   />
 </T.Mesh>
-<Model position={[0, 1.2, 0]} scale={1} />
+
+<Model position={[3, 3, 7]} rotation={[-0.5, 0.5, 0]} scale={[1, 1, 1]} />
 
 <Ground />
-
-<Particle />
-
 
 {#if debugEnabled === true}
   <Debug />
--- src/lib/right-rudder/phys/Ground.svelte
+++ src/lib/right-rudder/phys/Ground.svelte
@@ -3,13 +3,17 @@
 <script lang="ts">
   import { T } from '@threlte/core'
   import { AutoColliders } from '@threlte/rapier'
+
+	const color = 'gray'
+
 </script>
 
 <T.Group position={[0, -0.5, 0]}>
   <AutoColliders shape={'cuboid'}>
     <T.Mesh receiveShadow>
       <T.BoxGeometry args={[10, 1, 10]} />
-      <T.MeshStandardMaterial />
+      <T.MeshStandardMaterial color={color} />
     </T.Mesh>
   </AutoColliders>
 </T.Group>
+
