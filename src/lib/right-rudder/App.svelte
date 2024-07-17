69afe37d46ee0e7ceddcd2de713e83f0dd9de691
hackiku <ivan@pipewriter.io>
2024-07-17 02:34:02 +0200

    segments layout-ish

--- src/lib/right-rudder/App.svelte
+++ src/lib/right-rudder/App.svelte
@@ -1,4 +1,5 @@
 <!-- src/lib/right-rudder/App.svelte -->
+
 <script lang="ts">
   import { Canvas } from '@threlte/core';
   import Scene from './Scene.svelte';
@@ -23,74 +24,43 @@
 </Canvas>
 
 <div class="absolute top-0 left-0 p-4 text-white">
-  <h1 class="text-md font-bold">right rudder</h1>
+  <h1 class="text-2xl">right rudder</h1>
 </div>
 
 {#if $showControls}
   <!-- Left Segment -->
-  <div class="segment left-segment bg-gray-800 bg-opacity-30 rounded-lg p-4 m-4 hidden md:block">
-    <!-- Left Segment Content -->
+  <div class="flex flex-col items-start bg-gray-800 bg-opacity-30 rounded-lg p-4 m-4 absolute top-1/2 transform -translate-y-1/2 left-0 w-20vh h-60vh">
+    <div>airspeed = </div>
+    <div>foo = </div>
   </div>
-  <!-- Right Segment-->
-  <div class="segment right-segment bg-gray-800 bg-opacity-30 rounded-lg p-4 m-4 hidden md:block">
+
+  <!-- Right Segment -->
+  <div class="flex flex-col items-start bg-gray-800 bg-opacity-30 rounded-lg p-4 m-4 absolute top-1/2 transform -translate-y-1/2 right-0 w-20vh h-60vh">
     <!-- Right Segment Content -->
   </div>
+	
   <!-- Bottom Segment -->
-  <div class="segment bottom-segment bg-gray-800 bg-opacity-30 rounded-lg p-4 m-4 w-full md:w-2/3 lg:w-1/2">
+  <div class="flex items-center justify-center bg-gray-800 bg-opacity-30 rounded-lg p-4 m-4 w-full md:w-2/3 lg:w-1/2 absolute bottom-0 left-1/2 transform -translate-x-1/2">
     <StartStopButton on:toggle={handleToggle} />
   </div>
 {/if}
 
-<button on:click={toggleControls} class="toggle-button">
+<button on:click={toggleControls} class="absolute bottom-10 right-10 p-4 bg-gray-700 text-white rounded-lg">
   Toggle Controls
 </button>
 
 <style>
-  .segment {
-    background-color: rgba(55, 65, 81, 0.1);
-  }
 
-  .left-segment,
-  .right-segment {
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    width: 15vh;
-    height: 60vh;
-  }
+	div {
+		@apply text-white;
+	}
 
-  .left-segment {
-    left: 0;
-  }
+  h1 {
+		@apply text-2xl;
+	}
 
-  .right-segment {
-    right: 0;
-  }
-
-  .bottom-segment {
-    position: absolute;
-    bottom: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    height: 100px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .toggle-button {
-    position: absolute;
-    bottom: 10px;
-    right: 10px;
-    padding: 10px 20px;
-    background-color: gray;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-
-  .toggle-button:active {
-    background-color: darkgray;
-  }
+	p {
+		@apply text-sm text-white;
+	}
+	
 </style>
